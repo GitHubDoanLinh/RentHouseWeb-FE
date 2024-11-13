2c95d0526f3939ea4fd27855f5c6206d55ebe6c5
Doan Huu Linh <doanlinh1510@gmail.com>
2024-11-13 14:58:31 +0700

    demo2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Routes>
+        <Route path={"house"} element={<Home />}>
+          {/*<Route path={'add'} element={<AddProduct/>}/>*/}
+          {/*<Route path={'edit/:id'} element={<UpdateProduct/>}/>*/}
+          {/*<Route path={':id'} element={<ProductDetail/>}/>*/}
+        </Route>
+        <Route path="*" element={<Navigate to="houses/list" />} />
+        {/*dieu huong trang khi nhap sai*/}
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <>
+      <p
+        style={{
+          textAlign: "center",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        THUE NHA LUXURY 
+      </p>
+    </>
+  );
+}
