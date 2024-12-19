a97c1b7aeab98f565d8ef060e5eee0f8cffb5fc0
Doan Huu Linh <doanlinh1510@gmail.com>
2024-12-19 22:56:33 +0700

    fix update house

--- src/pages/houses/create/CreateHouse.js
+++ src/pages/houses/create/CreateHouse.js
@@ -62,7 +62,7 @@ export default function CreateHouse() {
               id: 0,
             },
             user: {
-              id: 1,
+              id: 0,
             },
           }}
           onSubmit={handleNext}
