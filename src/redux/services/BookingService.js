2dc4393e44a251c6ede8834bb25cfa182aaf8229
Doan Huu Linh <doanlinh1510@gmail.com>
2024-12-20 00:27:33 +0700

    thiet ke form booking house,gui api tao booking

---
+++ src/redux/services/BookingService.js
@@ -0,0 +1,9 @@
+import {createAsyncThunk} from "@reduxjs/toolkit";
+import getAxios from "./customAxios";
+export const addBooking = createAsyncThunk(
+    'booking/addBooking',
+    async (data) => {
+        let response = await getAxios().post('/booking',data);
+        return response.data;
+    }
+)
--- src/redux/store/Store.js
+++ src/redux/store/Store.js
@@ -3,12 +3,15 @@ import UserReducer from "../reducers/UserReducer";
 import houseReducer from "../reducers/HouseReducer";
 import categoryReducer from "../reducers/CategoryReducer";
 import imageReducer from "../reducers/ImageReducer";
+import bookingReducer from "../reducers/BookingReducer";
+
 export const store = configureStore ({
     reducer: {
         users: UserReducer,
         houses : houseReducer,
         categories: categoryReducer,
-        images : imageReducer
+        images : imageReducer,
+        bookings: bookingReducer
     }
 })
 export default store;
